e77b34ba79d72263cab02a25b877df5d176a07e4
florestan1981 <81873271+florestan1981@users.noreply.github.com>
2021-05-05 17:58:03 +0300

    Add files via upload

---
+++ staticfiles/js/animations.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+    $('.js--graph-area').waypoint(function(direction) {
+        $('.js--graph-area').addClass('animate__animated animate__fadeInLeft');
+    }, {
+        offset: '50%'
+    });
+
+});
+
+
---
+++ staticfiles/js/carousel.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+    $('.carousel').carousel({
+        interval: 2000,
+        pause: "false"
+
+      })
+
+});
