bf810ee7ec0726aff0cdd9fab6f4e2206e03191c
florestan1981 <81873271+florestan1981@users.noreply.github.com>
2021-09-16 19:29:46 +0300

    Update settings.py

--- Shambala/settings.py
+++ Shambala/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'lm6zn0l%v&dyab8+0wbmzv(!7+9+ur7c#od4j)4zh7drksi5pi'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
